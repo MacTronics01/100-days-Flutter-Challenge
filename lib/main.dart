d9346056f695953512073855d7906dfc2f6ae6d5
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 <ubonmacaulay@gmail.com>
2023-05-02 22:36:01 +0100

    day9

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_challange/screens/day8.dart';
+import 'package:flutter_challange/screens/day9.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home:   const Day8DraggableScrollableSheet(key: null,),
+      home:   const Day9AbsorbPointer(key: null,),
     );
   }
 }
